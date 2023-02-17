8914c4f54f3c55f24b48f42c0549c2d869ef0b8b
Yura <yout54702@gmail.com>
2023-02-16 19:27:05 -0800

    first commit

---
+++ kbvbhhv-m1/resources/views/katalog.blade.php
@@ -0,0 +1,35 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/resources/css/style/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+
+
+
+<div class="map">
+    <h1>Наше местоположение</h1>
+    <img src="/resources/img/map.jpg" alt="img" width="50%" height="50%">
+</div>
+
+
+
+
+
+
+
+
+</body>
+</html>
